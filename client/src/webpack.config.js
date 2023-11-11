1ee6c4a612801002bce92d2243682430b9371b2e
Sara Garcia <saragca81@gmail.com>
2023-11-11 15:07:59 -0800

    added code to config file

--- client/src/webpack.config.js
+++ client/src/webpack.config.js
@@ -18,11 +18,25 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swMode: 'src-sw.js',
+      }),
+      new WebpackPwaManifest({
+
+      })
       
     ],
 
     module: {
       rules: [
+        {
+          
+        }
         
       ],
     },
