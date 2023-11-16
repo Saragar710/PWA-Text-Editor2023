f9541fb804e6389017df0e2c8d76ef725d327dfd
Sara Garcia <saragca81@gmail.com>
2023-11-16 12:25:07 -0800

    refactored database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,8 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 
-export const putDb = async (content) => console.error('putDb not implemented');{
+export const putDb = async (content) => {
+console.error('putDb not implemented');
 // export const putDb = async (id, content) => {
   console.log('PUT to the database');
   const jateDb = await openDB('jate', 1);
@@ -35,7 +36,7 @@ export const getDb = async () => console.error('getDb not implemented');{
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.get(id);
+  const request = store.getAll();
   const result = await request;
   console.log('result.value', result);
   return result;
